429fcf3bbfae318c2aa30432f380f1e71f3b9a3d
darkgeneration77-spec <darkgeneration77@gmail.com>
2026-07-22 13:37:00 +0800

    Load staging review styles

--- web-app/app/staging/page.tsx
+++ web-app/app/staging/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation';
 import { createClient } from '../../lib/supabase/server';
 import { approveStaging, rejectStaging, updateStaging } from './actions';
+import './staging.css';
 
 export default async function StagingPage(){
   const supabase=await createClient();
